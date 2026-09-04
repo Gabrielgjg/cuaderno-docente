3e62cfbb06579c885e62e938b968d13aa9193c51
Gabrielgjg <72362416+Gabrielgjg@users.noreply.github.com>
2026-09-03 19:00:34 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cuaderno-docente-v6';
+const CACHE = 'cuaderno-docente-v7';
 const SHELL = ['./', './index.html', './app.js', './admin.js', './classroom.js', './chart.umd.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
