67cb4a792a657ecf6f84bdd54532161f66d0bc86
Gabrielgjg <72362416+Gabrielgjg@users.noreply.github.com>
2026-09-08 22:47:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const CONFIG = {
   // Pega aquí la URL /exec de tu implementación de Apps Script
   API_URL: 'PEGA_AQUI_TU_URL_DE_APPS_SCRIPT_/exec',
   CICLO: '2026-2027',
-  APP_VERSION: 'v21'
+  APP_VERSION: 'v22'
 };
 
 const ESTATUS_ASISTENCIA = ['Presente', 'Ausente', 'Retardo', 'Justificado'];
