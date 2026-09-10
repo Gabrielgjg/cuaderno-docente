f69572c89b6c34c91ed864fbebd863dd57aec835
Gabrielgjg <72362416+Gabrielgjg@users.noreply.github.com>
2026-09-10 08:08:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const CONFIG = {
   // Pega aquí la URL /exec de tu implementación de Apps Script
   API_URL: 'PEGA_AQUI_TU_URL_DE_APPS_SCRIPT_/exec',
   CICLO: '2026-2027',
-  APP_VERSION: 'v27'
+  APP_VERSION: 'v28'
 };
 // Restaura la URL guardada ANTES de cualquier intento de conexión al arrancar
 (function () {
